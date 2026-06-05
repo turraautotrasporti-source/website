d6d4a46c954ea3aeb3bcc404bdd6f8f2bbcb3ece
turraautotrasporti-source <turra.autotrasporti@gmail.com>
2026-06-05 13:58:38 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const elements = document.querySelectorAll('.reveal');
+
+window.addEventListener('scroll', () => {
+    elements.forEach(el => {
+        const top = el.getBoundingClientRect().top;
+        if(top < window.innerHeight - 100){
+            el.classList.add('active');
+        }
+    });
+});
