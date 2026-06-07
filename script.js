3baec2b77110e921e1e1e6700265fd03944955ae
turraautotrasporti-source <turra.autotrasporti@gmail.com>
2026-06-07 11:05:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,25 @@ window.addEventListener('scroll', () => {
 });
 
 
+// LIGHTBOX GALLERY
+const galleryImages = document.querySelectorAll('.gallery img');
+const lightbox = document.getElementById('lightbox');
+const lightboxImg = document.getElementById('lightbox-img');
+const closeBtn = document.querySelector('.close');
+
+galleryImages.forEach(img => {
+    img.addEventListener('click', () => {
+        lightbox.style.display = 'flex';
+        lightboxImg.src = img.src;
+    });
+});
+
+closeBtn.addEventListener('click', () => {
+    lightbox.style.display = 'none';
+});
+
+lightbox.addEventListener('click', (e) => {
+    if (e.target === lightbox) {
+        lightbox.style.display = 'none';
+    }
+});
